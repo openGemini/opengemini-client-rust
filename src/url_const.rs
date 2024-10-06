041750cc84dc2c7a68ceedee383c743be68ac3a2
ZhangJian He <shoothzj@gmail.com>
2024-10-06 18:27:48 +0800

    feat: add url const (#10)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

---
+++ src/url_const.rs
@@ -0,0 +1,4 @@
+pub const URL_PING: &'static str = "/ping";
+pub const URL_QUERY: &'static str = "/query";
+pub const URL_STATUS: &'static str = "/status";
+pub const URL_WRITE_OUTPUT: &'static str = "/write";
