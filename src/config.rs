0dfcfa6362cd8db45308426fd76a3b31b8cbce91
cpcchengt <chengt@cpchain.io>
2024-10-08 04:15:09 +0800

    feat: add client trait and config struct
    Signed-off-by: cpcchengt <chengt@cpchain.io>
    Co-authored-by: ZhangJian He <shoothzj@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -12,8 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod config;
-mod opengemini_client;
-mod url_const;
-
-pub use opengemini_client::Client;
+pub struct Config {}
