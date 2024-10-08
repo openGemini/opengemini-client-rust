8d718aa0aba13a8fae3cf39b9e0fce3c84fff075
cpcchengt <chengt@cpchain.io>
2024-10-09 00:28:27 +0800

    build: func for create client & modify config
    Signed-off-by: cpcchengt <chengt@cpchain.io>

--- src/opengemini_client.rs
+++ src/opengemini_client.rs
@@ -12,14 +12,58 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::config::Config;
+use std::sync::atomic::{AtomicBool, AtomicI32};
+
+use crate::config::{Address, Config};
+use crate::error::ClientError;
+use reqwest::Client as HttpClient;
+
+pub struct Endpoint {
+    pub url: String,
+    pub is_down: AtomicBool,
+}
 
 pub struct Client {
     config: Config,
+    endpoints: Vec<Endpoint>,
+    cli: HttpClient,
+    prev_idx: AtomicI32,
 }
 
 impl Client {
-    pub fn new(config: Config) -> Self {
-        Client { config }
+    pub fn new(cfg: &Config) -> Self {
+        let client = HttpClient::new();
+        let endpoints = build_endpoints(cfg.address.clone());
+        Client {
+            config: cfg.clone(),
+            endpoints,
+            cli: client,
+            prev_idx: AtomicI32::new(0),
+        }
     }
+
+    pub fn ping(&self) -> Result<(), ClientError> {
+        todo!();
+    }
+
+    pub fn query(&self) -> Result<(), ClientError> {
+        todo!();
+    }
+
+    pub fn close(&self) -> Result<(), ClientError> {
+        todo!();
+    }
+}
+
+pub fn build_endpoints(addresses: Vec<Address>) -> Vec<Endpoint> {
+    addresses
+        .into_iter()
+        .map(|addr| {
+            let url = format!("http://{}:{}", addr.host, addr.port);
+            Endpoint {
+                url,
+                is_down: AtomicBool::new(false),
+            }
+        })
+        .collect()
 }
