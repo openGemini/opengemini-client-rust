e97a6366458e08c95fc8c9418ab03f61ccb47a7b
yexuanruche <85327690+cpcchengt@users.noreply.github.com>
2024-10-10 10:20:39 +0800

    build: ping and get server url (#15)
    Signed-off-by: cpcchengt <chengt@cpchain.io>

--- src/opengemini_client.rs
+++ src/opengemini_client.rs
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::atomic::{AtomicBool, AtomicI32};
+use std::sync::atomic::{AtomicBool, AtomicI32, Ordering};
 
 use crate::config::{Address, Config};
 use crate::error::ClientError;
+use crate::url_const::URL_PING;
 use reqwest::Client as HttpClient;
 
 pub struct Endpoint {
@@ -42,8 +43,21 @@ impl Client {
         }
     }
 
-    pub fn ping(&self) -> Result<(), ClientError> {
-        todo!();
+    pub fn ping(&self, idx: usize) -> Result<bool, ClientError> {
+        if idx >= self.endpoints.len() as usize {
+            return Err(ClientError::ValueError("Index out of range".to_string()));
+        }
+        let url: String = self.endpoints[idx].url.clone() + URL_PING;
+        match reqwest::blocking::get(url) {
+            Ok(response) => {
+                if response.status().is_success() {
+                    return Ok(true);
+                } else {
+                    return Ok(false);
+                }
+            }
+            Err(_) => Err(ClientError::ConnectionError),
+        }
     }
 
     pub fn query(&self) -> Result<(), ClientError> {
@@ -52,6 +66,22 @@ impl Client {
 
     pub fn close(&self) -> Result<(), ClientError> {
         todo!();
+    }
+
+    fn get_server_url(&self) -> Option<String> {
+        let current_index = self.prev_idx.load(Ordering::SeqCst);
+        let endpoint_count = self.endpoints.len() as i32;
+        let url = if endpoint_count > 0 {
+            let url = self.endpoints[current_index as usize % endpoint_count as usize]
+                .url
+                .clone();
+            self.prev_idx
+                .store((current_index + 1) % endpoint_count, Ordering::SeqCst);
+            Some(url)
+        } else {
+            None
+        };
+        url
     }
 }
 
